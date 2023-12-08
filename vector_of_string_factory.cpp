027ad881f1e88ae3d37ab66fc651734f56997466
Ahmed Al-Ganad <ahmedbaligh100@gmail.com>
2023-12-08 17:18:44 +0100

    vector of string added

---
+++ vector_of_string_factory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ahmed on 08/12/2023.
+//
+
+#include "vector_of_string_factory.h"
---
+++ vector_of_string_factory.h
@@ -0,0 +1,15 @@
+//
+// Created by ahmed on 08/12/2023.
+//
+
+#ifndef APC_BINARY_FILES_EXC_VECTOR_OF_STRING_FACTORY_H
+#define APC_BINARY_FILES_EXC_VECTOR_OF_STRING_FACTORY_H
+#include "ifactory.h"
+
+template<typename T>
+class vector_of_string_factory : public ifactory<std::vector<std::string>> {
+    T construct_from_string(const std::string& s) override;
+
+};
+
+#endif //APC_BINARY_FILES_EXC_VECTOR_OF_STRING_FACTORY_H
